70d440f59afef72b7615d878760915af8a797cda
SABBIR SHARIF <149605755+sabbir-sharif@users.noreply.github.com>
2024-12-12 21:40:48 +0600

    Delete loginDemo directory

--- loginDemo/src/main/java/com/loginDemo/repository/OwnerRegiRepository.java
+++
@@ -1,9 +0,0 @@
-package com.loginDemo.repository;
-
-import com.loginDemo.model.Owner;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OwnerRegiRepository extends JpaRepository<Owner, Integer> {
-
-    public boolean existsByUserName(String userName);
-}
--- loginDemo/src/main/java/com/loginDemo/repository/OwnerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.loginDemo.repository;
-
-import com.loginDemo.model.Property;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OwnerRepository extends JpaRepository<Property, Integer> {
-}
